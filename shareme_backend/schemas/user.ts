391aaf12212ef8ad75503fc717531eef7df99c66
Udith Shyamsukha <udithshyamsukha@gmail.com>
2023-05-11 13:57:53 +0530

    Sanity setup

---
+++ shareme_backend/schemas/user.ts
@@ -0,0 +1,17 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'userName',
+            title: 'UserTitle',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string'
+        }
+    ]
+}
